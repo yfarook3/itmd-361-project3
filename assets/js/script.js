1ef36edb76cff0b1f3c6c2ad7da900699790078c
yfarook3 <60125652+yfarook3@users.noreply.github.com>
2020-04-24 10:52:47 -0500

    images
    Adding images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 var map;
   function initMap() {
+	  myCoordinates={
+		  lat: 41.891263
+		  lng:  -87.619089
+	  }
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.891263, lng:  -87.619089},
-          zoom: 8
-        });
-      }
-    </script>
+          center: myCoordinates
+          zoom: 13
+		});
+
+		var amcRiver = new google.maps.Marker({
+				map: map,
+				title: "The theatre I watched Avengers: Endgame at.."
+		});
+  }	
+
+amcRiver.setMap(map);
+window.addEventListener('load', initMap);
+
+//for the slide show
+var i=0;
+var images =[];
+var time = 2000;
+
+images[0]= "assets/images/capmap.jpg";
+images[1]="assets/images/ironman.jpg";
+images[2]= ""assets/images/tho.jpg";  
+images[3]="assets/images/hulk.jpg";
+
+funct
